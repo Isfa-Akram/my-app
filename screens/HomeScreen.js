1da2df540c4bc5a50bf45dbbca3448b4e0e89ecd
codaestroTayyab <tayyab.saeed1@gmail.com>
2023-02-09 00:33:58 +0500

    home working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -26,6 +26,7 @@ const HomeScreen = ()=>{
         onPress={(data, details = null) => {
 
           console.log(data);
+          console.log(details);
           
            dispatch(setOrigin({
             location: details.geometry.location,
